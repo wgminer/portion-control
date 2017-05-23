5ef0297b38961c143fb68d4c17e008c552fdcb96
Will Miner <wgminer@gmail.com>
2017-05-23 10:03:19 -0400

    Most recent work

--- src/js/inject.js
+++ src/js/inject.js
@@ -8,21 +8,21 @@ chrome.storage.sync.get('snoozeDuration', function(items) {
 	}
 });
 	
-function countdown (sec) {
-	var $el = $('#remove-overlay');
-    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
-    function timer () {
-      	sec-=1;
-      	if (sec <= 0) {
-        	clearInterval(counter);
-        	$el.removeClass('is--loading');
-        	//counter ended, do something here
-        	return false;
-      	}
-      	$el.find('.countdown').text(sec);
-      	//Do code for showing the number of seconds here
-    }
-}
+// function countdown (sec) {
+// 	var $el = $('#remove-overlay');
+//     var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+//     function timer () {
+//       	sec-=1;
+//       	if (sec <= 0) {
+//         	clearInterval(counter);
+//         	$el.removeClass('is--loading');
+//         	//counter ended, do something here
+//         	return false;
+//       	}
+//       	$el.find('.countdown').text(sec);
+//       	//Do code for showing the number of seconds here
+//     }
+// }
 
 function showOverlay (url, veggies) {
 
@@ -37,51 +37,38 @@ function showOverlay (url, veggies) {
 	
     var body = document.getElementsByTagName('body')[0];
 	body.style.overflow = 'hidden';
-	body.innerHTML += '<div id="portion-control"><p class="brand">Portion Control</p><div><h1>' + title + '</h1><ul></ul><button id="remove-overlay" class="is--loading"><span class="countdown"></span><span class="fine">Nah, not right now</span></button></div></div>';
-    
-    // if (veggies.length > 3) {
-    //     veggies = _.sample(veggies, 3);
-    // }
-
-    veggies.forEach(function (veg) {
-
-        $.get('//opengraph.io/api/1.0/site/' + encodeURI(veg) + '?app_id=5853677981a0d71a2e1c7986', function (data) {
-            console.log(data);
-            var item = `<li class="veggie">
-                <a href="` + veg + `">
-                    <h2 class="veggie__title">` + data.hybridGraph.site_name + `</h2>
-                    <p class="veggie__description">` + data.hybridGraph.description.split('. ')[0] + `.</p>
-                </a>
-            </li>`;
-            $('#portion-control ul').append(item);
-        });
-    });
+	body.innerHTML += '<div id="pc"><div class="pc-container"><div id="pc-1" class="pc-sentence"></div></div></div>';
 
 	setTimeout(function(){
-	   	document.getElementById("remove-overlay").addEventListener('click', function () {
-	   		removeOverlay(url);
-	   	});
+		Typed.select('.pc-sentence', {
+            strings: ['Want to do something healthy instead?'],
+            typeSpeed: 0,
+            showCursor: false
+        });
+	   	// document.getElementById("remove-overlay").addEventListener('click', function () {
+	   	// 	removeOverlay(url);
+	   	// });
 	}, 10);
 }
 
-function removeOverlay (url) {
-	var body = document.getElementsByTagName('body')[0];
-	body.style.overflow = '';
-	document.getElementById('portion-control').remove();
+// function removeOverlay (url) {
+// 	var body = document.getElementsByTagName('body')[0];
+// 	body.style.overflow = '';
+// 	document.getElementById('portion-control').remove();
 
-	chrome.storage.sync.get('snoozed', function(items) {
-		var snoozed = items.snoozed 
-		if (typeof snoozed == 'undefined') snoozed = {};
-	    snoozed[url] = Date.now() + snoozeDuration;
-	    chrome.storage.sync.set({
-	        snoozed: snoozed
-	    }, function() {
-	        console.log(snoozed);
-	    });
+// 	chrome.storage.sync.get('snoozed', function(items) {
+// 		var snoozed = items.snoozed 
+// 		if (typeof snoozed == 'undefined') snoozed = {};
+// 	    snoozed[url] = Date.now() + snoozeDuration;
+// 	    chrome.storage.sync.set({
+// 	        snoozed: snoozed
+// 	    }, function() {
+// 	        console.log(snoozed);
+// 	    });
 
-	});
+// 	});
 
-}
+// }
 
 function isSnoozed (snoozed, url) {
 	var now = Date.now();
